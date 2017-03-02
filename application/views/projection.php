5e9928332957dd6fa886e7f01a6b7bc5d1f7f5c8
GHARESLLAOUI Med Yacine <BS5083@sdmn01.sirius.infra.com>
2017-03-02 18:07:21 +0100

    correction

--- application/views/projection.php
+++ application/views/projection.php
@@ -3,11 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 include('include/head.php');
 ?>﻿
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/jquery.dataTables.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/jqc-1.12.4/dt-1.10.13/datatables.min.css"/>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs-3.3.7/jqc-1.12.4/dt-1.10.13/datatables.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/jquery.table2excel.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/jquery.dataTables.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/jquery-ui-1.10.4.custom.min.js"></script>
+
 
 <script type="text/javascript">
     var dataTable = <?php echo $dataTable; ?>;
