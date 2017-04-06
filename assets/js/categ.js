af953241431d83570d9f77004699f5d9c8df907f
GHARESLLAOUI Med Yacine <BS5083@sdmn01.sirius.infra.com>
2017-04-07 00:31:53 +0200

    correction

--- assets/js/categ.js
+++ assets/js/categ.js
@@ -2,7 +2,11 @@ $(document).ready(function () {
 
     refreshDataCat();
     loadCateg();
-    loadSousCategorie();
+    refreshSC(0);
+    $('#list-bib').change(function () {
+        refreshSC($(this).val());
+    });
+
     function refreshDataCat() {
         var tbody = $('<tbody></tbody>').empty();
         $.each(data_categs, function (idObj, valData) {
@@ -38,40 +42,40 @@ $(document).ready(function () {
         });
 
     }
-    function loadSousCategorie() {
-        $('#list-bib').change(function () {
-            refreshSC($(this).val());
-        });
-    }
-    function refreshSC(id){
+
+    function refreshSC(id) {
+        if (id == 0) {
+            id = $('#list-bib').val();
+        }
         $.ajax({
-           url: "/list-sc",
-           data: {"id_cat":id},
-           
-        }).done(function(resp){
-              var tbody = $('<tbody></tbody>').empty();
-        $.each(data_categs, function (idObj, valData) {
-            var trData = $('<tr></tr>');
-            trData.append($('<td>' + valData["lib_categ"] + '</td>'));
-            trData.append($('<td>' + valData["commentaire"] + '</td>'));
-            trData.append($('<td>' + valData["username"] + '</td>'));
-            trData.append($('<td>' + valData["added_at"] + '</td>'));
+            type: "post",
+            url:  base_url + "index.php/list-sc",
+            data: {id_cat: id},
 
-            trData.append($('<td>').append('<button type="button" class="btn btn-danger btn-sm btn-round"><span class="glyphicon glyphicon-trash"></span></button></td>').click(function () {
-                if (confirm('Vous etes sur le point de supprimer ' + valData["lib_categ"])) {
-                    document.location.href = "delete-categ/" + valData["lib_categ_id"];
-                } else {
-                    return;
-                }
-            }));
+        }).done(function (resp) {
+            var tbody = $('<tbody></tbody>').empty();
+            $.each(resp, function (idObj, valData) {
+                var trData = $('<tr></tr>');
+                trData.append($('<td>' + valData["lib_sous_categ‏_nom"] + '</td>'));
+                trData.append($('<td>' + valData["lib_sous_categ‏_desc"] + '</td>'));
+                trData.append($('<td>' + valData["username"] + '</td>'));
+                trData.append($('<td>' + valData["added_at"] + '</td>'));
+
+                trData.append($('<td>').append('<button type="button" class="btn btn-danger btn-sm btn-round"><span class="glyphicon glyphicon-trash"></span></button></td>').click(function () {
+                    if (confirm('Vous etes sur le point de supprimer ' + valData["lib_sous_categ‏_nom"])) {
+                        document.location.href = "delete-categ/" + valData["lib_sous_categ‏_id"];
+                    } else {
+                        return;
+                    }
+                }));
 
 
-            tbody.append(trData);
-        });
-        $("#tabCat").find("tbody").remove();
-        $("#tabCat").append(tbody);
-        $('.pager').hide();
-        paginate("#tabCat", 'tbody tr', 6);    
+                tbody.append(trData);
+            });
+            $("#sou_bib").find("tbody").remove();
+            $("#sou_bib").append(tbody);
+            $('.pager').hide();
+            paginate("#sou_bib", 'tbody tr', 6);
         });
     }
 });
