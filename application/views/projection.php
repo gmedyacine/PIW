67608131d3d7668c1431da3859cb95b457642133
Nadia HABOURIA <nadia-habouria@hotmail.com>
2017-05-17 15:37:45 +0100

    add new report category

--- application/views/projection.php
+++ application/views/projection.php
@@ -8,6 +8,7 @@ include('include/dataTables.php');
     var dataTable = <?php echo $dataTable; ?>;
     var lastDate = <?php echo (string) $lastDate; ?>;
     var dataNameColonne = <?php echo $dataNameColonne; ?>;
+    var report_categ_json = <?php echo $report_categ_json; ?>;
 </script>
 <body>
 
@@ -18,6 +19,7 @@ include('include/dataTables.php');
         <div class="row content">
             <!-- Colonne du Menu -->
 <?php include('include/menu.php'); ?>
+            <?php echo $this->session->flashdata('msg'); ?>
 
             <div class="col-md-9"> <!-- Début partie du tableau -->
                 <fieldset class="group-border">
@@ -41,7 +43,56 @@ include('include/dataTables.php');
                         </div>
                     </div>
                 </fieldset>
+                
+                                    <div class="row">
+                                                            <div class="panel panel-default panel-set">
+                        <div class="panel-heading">
+                           <?php echo $this->lang->line("assigner_categ"); ?>
+                        </div>
+                    
+                        <div class="panel-body"> 
+
+                            <div class="form-group">    
+                              
+                                <form action="" method="post">
+                                <div class="col-sm-10">
+                                    <select id="select_report_categ" class="form-control" >
+                                           <option value="0">-- <?php echo $this->lang->line('select_categ'); ?> --</option>
+                                       </select>
+                                    <p style="font-size: 12px; display: inline;"><?php echo $this->lang->line('categ_not_found'); ?></p><a href="#" id="add_categ" style="font-size: 12px; display: inline;"> <?php echo $this->lang->line('create_categ'); ?></a>
+                                    <br>
+                                </div>
+                                <div class="col-sm-2">
+                                    <a href="#" id="btn_select_report_categ" class="btn btn-info pull-right"><?php echo $this->lang->line('valider'); ?> </a>
+                                </div>
+                                </form>    
+                               
+                            </div>
+                           
+                            <div id="addReportCat" class="form-group">  
+                                <?php echo form_open('home/add_report_categ'); ?>
+                                  <form action="" method="post">  
+                                <div class="col-sm-10">
+                               <br>
+                               <input type="text" name="nom_report_categ" required="required" class="form-control">
+                                     <br>
+                                    <input type="hidden" name="id_projection" value="<?php echo $id_projection; ?> "> 
+                                </div>
+                                <div class="col-sm-2">
+                                 <br>
+                                   <input type="submit" id="btn_add_categ" class="btn btn-info pull-right" value="<?php echo $this->lang->line('add_categ'); ?> ">
+                                    
+                                </div>
+                                 </form> 
+                            </div>
+                          
+                        </div>
+                    </div>
+                  
+                    </div>
+                
                 <div class="row ">       <!-- Début titre du tableau et lien export excel-->
+                    
                     <div id="panel-table" class="panel panel-default panel-reduit-5">
                         <div class="panel-body">
                             <div class="col-6 pull-left ">
@@ -50,7 +101,7 @@ include('include/dataTables.php');
                             <div class="col-6 pull-right dropdown">
                                 <button class="btn icon-btn btn-success dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
                                     <span class="glyphicon btn-glyphicon glyphicon-share img-circle text-info"></span>
-                                    Export Excel 
+                                   <?php echo $this->lang->line("export_excel"); ?>  
                                     <span class="caret"></span>
                                 </button>
 
@@ -81,7 +132,20 @@ include('include/dataTables.php');
 
             </div> <!-- fin pagination  -->
             <script src="<?php echo base_url(); ?>assets/js/projection.js"></script>
+             <script src="<?php echo base_url(); ?>assets/js/reportCateg.js"></script>
             <script>$('.dropdown-toggle').dropdown()</script>
+                <script type="text/javascript">
+
+    $(document).ready(function () {
+
+        window.setTimeout(function () {
+            $(".alert").fadeTo(1000, 0).slideUp(1000, function () {
+                $(this).remove();
+            });
+        }, 1000);
+
+    });
+    </script>
 
         </div> <!-- Fin partie du tableau -->
         <!-- ROW END -->
