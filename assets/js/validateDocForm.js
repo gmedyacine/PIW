a695916acf834651f01af5ea1a4b5f1b4e979b35
Nadia HABOURIA <nadia-habouria@hotmail.com>
2017-06-07 14:12:02 +0100

    Add Doc Validation

---
+++ assets/js/validateDocForm.js
@@ -0,0 +1,41 @@
+/**
+ * Created by dell on 07/06/2017.
+ */
+$(document).ready(function(){
+    $.validator.setDefaults({
+        errorClass:'help-block',
+        highlight: function(element){
+            $(element)
+                .closest('.form-control')
+                .addClass('has-error')
+        },
+        unhighlight: function(element){
+            $(element)
+                .closest('.form-control')
+                .removeClass('has-error')
+        }
+
+    });
+$("#addDocForm").validate({
+    rules: {
+        calender: "required",
+        libCat: "required",
+        libSousCat: "required",
+        job: "required",
+        vega: "required",
+        heureLib: "required",
+        newFile: "required"
+        
+    },
+    messages: {
+        calender: msg,
+        libCat: msg,
+        libSousCat: msg,
+        job: msg,
+        vega:msg,
+        heureLib: msg,
+        newFile: msg 
+
+    }
+});
+});
